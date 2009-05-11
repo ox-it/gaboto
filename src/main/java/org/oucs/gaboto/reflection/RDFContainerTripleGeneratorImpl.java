70438828bf87ccda2bb664b135d3c18b1070738e
Tim Pizey <tim.pizey@oucs.ox.ac.uk>
2009-05-11 16:29:06 +0000

    Unused import

--- src/main/java/org/oucs/gaboto/reflection/RDFContainerTripleGeneratorImpl.java
+++ src/main/java/org/oucs/gaboto/reflection/RDFContainerTripleGeneratorImpl.java
@@ -35,7 +35,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 
 import org.apache.log4j.Logger;
