c7449635ec82f12c37cfe539de589cf59e7f5272
Tim Pizey <tim.pizey@oucs.ox.ac.uk>
2009-05-11 15:37:04 +0000

    Refactor packaging

---
+++ src/test/java/org/oucs/gaboto/model/test/GabotoTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2009 University of Oxford
+ *
+ * Written by Tim Pizey for the Erewhon Project
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *  - Neither the name of the University of Oxford nor the names of its 
+ *    contributors may be used to endorse or promote products derived from this 
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * 
+ */
+package org.oucs.gaboto.model.test;
+
+import java.util.Collection;
+
+import org.oucs.gaboto.GabotoConfiguration;
+import org.oucs.gaboto.GabotoLibrary;
+import org.oucs.gaboto.model.Gaboto;
+import org.oucs.gaboto.model.GabotoFactory;
+import org.oucs.gaboto.vocabulary.DC;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Tim Pizey
+ * @since 7 May 2009
+ *
+ */
+public class GabotoTest extends TestCase {
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    GabotoLibrary.init(GabotoConfiguration.fromConfigFile());
+  }
+
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+   * Test method for {@link org.oucs.gaboto.model.Gaboto#getEntityURIsFor(com.hp.hpl.jena.rdf.model.Property)}.
+   */
+  public final void testGetEntityURIsForProperty() {
+  }
+  public void testURIsForTitle() { 
+    Gaboto gaboto = GabotoFactory.getInMemoryGaboto();
+    Collection<String> uris = gaboto.getEntityURIsFor((DC.title));
+    for (String uri : uris) 
+      System.err.println(uri);
+  }
+
+}
