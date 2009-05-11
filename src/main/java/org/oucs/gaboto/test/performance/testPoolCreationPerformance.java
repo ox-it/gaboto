c0863d51e0698154046c29fa83bebadfe39c4541
Tim Pizey <tim.pizey@oucs.ox.ac.uk>
2009-05-11 15:35:12 +0000

    Remove unused import

--- src/main/java/org/oucs/gaboto/test/performance/testPoolCreationPerformance.java
+++ src/main/java/org/oucs/gaboto/test/performance/testPoolCreationPerformance.java
@@ -40,7 +40,6 @@ import org.oucs.gaboto.GabotoLibrary;
 import org.oucs.gaboto.entities.pool.GabotoEntityPool;
 import org.oucs.gaboto.entities.pool.GabotoEntityPoolConfiguration;
 import org.oucs.gaboto.exceptions.EntityPoolInvalidConfigurationException;
-import org.oucs.gaboto.exceptions.GabotoException;
 import org.oucs.gaboto.model.Gaboto;
 import org.oucs.gaboto.model.GabotoFactory;
 import org.oucs.gaboto.model.GabotoSnapshot;
