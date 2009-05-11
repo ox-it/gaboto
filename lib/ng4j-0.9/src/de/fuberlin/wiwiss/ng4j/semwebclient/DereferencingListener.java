d75c098f29f60f041e38534dd4efab9d918acfda
Tim Pizey <tim.pizey@oucs.ox.ac.uk>
2009-05-11 13:49:07 +0000

    Only hold jars, not whole project

--- lib/ng4j-0.9/src/de/fuberlin/wiwiss/ng4j/semwebclient/DereferencingListener.java
+++
@@ -1,9 +0,0 @@
-package de.fuberlin.wiwiss.ng4j.semwebclient;
-
-
-/**
- * Interface of a Listener which handles the DereferencingResults.
- */
-public interface DereferencingListener {
-	void dereferenced(DereferencingResult result);
-}
