069f64450cbcddfedd40d5175a9cd4aca99d76c8
Tim Pizey <tim.pizey@oucs.ox.ac.uk>
2009-06-17 13:14:32 +0000

    Simplify

--- src/main/java/org/oucs/gaboto/transformation/json/JSONPoolTransformer.java
+++ src/main/java/org/oucs/gaboto/transformation/json/JSONPoolTransformer.java
@@ -37,249 +37,253 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
-import org.json.JSONException;
-import org.json.JSONStringer;
+
+// FIXME This fails with duplicate keys if json.org library is used!!!!
+
+import net.sf.json.JSONException;
+import net.sf.json.util.JSONStringer;
 import org.oucs.gaboto.beans.GabotoBean;
 import org.oucs.gaboto.entities.GabotoEntity;
 import org.oucs.gaboto.entities.pool.GabotoEntityPool;
-import org.oucs.gaboto.exceptions.GabotoRuntimeException;
 import org.oucs.gaboto.transformation.EntityPoolTransformer;
 
 /**
  * Transforms a GabotoEntityPool into JSON.
  * 
  * @author Arno Mittelbach
- *
+ * 
  */
 public class JSONPoolTransformer implements EntityPoolTransformer {
 
-	private Map<GabotoEntity, Integer> levelMap = new HashMap<GabotoEntity, Integer>();
-	
-	/**
-	 * Collect all direct properties
-	 */
-	public static final int COLLECT_DIRECT_PROPERTIES = 1;
-	
-	/**
-	 * Collect all passive properties
-	 */
-	public static final int COLLECT_PASSIVE_PROPERTIES = 2;
-	
-	/**
-	 * Collect all indirect properties
-	 */
-	public static final int COLLECT_INDIRECT_PROPERTIES = 4;
-	
-	/**
-	 * The maximum allowed nesting level
-	 */
-	public static final int MAX_NESTING = 8;
-	
-	private int nesting = 1;
-	private int collectProperties = COLLECT_DIRECT_PROPERTIES | COLLECT_INDIRECT_PROPERTIES | COLLECT_PASSIVE_PROPERTIES;
-	
-	public String transform(GabotoEntityPool pool) {
-			return transfromEntities(pool.getEntities());
-	}
+  private Map<GabotoEntity, Integer> levelMap = new HashMap<GabotoEntity, Integer>();
 
-    private String simplifyKey(String k) { 
-	k = k.replaceAll("http://ns.ox.ac.uk/namespace/oxpoints/2009/02/owl#", "oxp_");
-	k = k.replaceAll("http://ns.ox.ac.uk/namespace/gaboto/kml/2009/03/owl#", "gab_");
-	k = k.replaceAll("http://purl.org/dc/elements/1.1/", "dc_");
-	k = k.replaceAll("http://nwalsh.com/rdf/vCard#", "vCard_");
-	k = k.replaceAll("http://www.opengis.net/gml/", "geo_");
-	return k;
+  /**
+   * Collect all direct properties
+   */
+  public static final int COLLECT_DIRECT_PROPERTIES = 1;
+
+  /**
+   * Collect all passive properties
+   */
+  public static final int COLLECT_PASSIVE_PROPERTIES = 2;
+
+  /**
+   * Collect all indirect properties
+   */
+  public static final int COLLECT_INDIRECT_PROPERTIES = 4;
+
+  /**
+   * The maximum allowed nesting level
+   */
+  public static final int MAX_NESTING = 8;
+
+  private int nesting = 1;
+  private int collectProperties = COLLECT_DIRECT_PROPERTIES
+      | COLLECT_INDIRECT_PROPERTIES | COLLECT_PASSIVE_PROPERTIES;
+
+  public String transform(GabotoEntityPool pool) {
+    Collection<GabotoEntity> entities = pool.getEntities();
+    
+    // Initialize level map
+    for (GabotoEntity entity : entities) {
+      levelMap.put(entity, new Integer(1));
     }
 
-	private String transfromEntities(Collection<GabotoEntity> entities) {
-      // initialize level map
-      for(GabotoEntity entity : entities){
-        levelMap.put(entity, new Integer(1));
+    JSONStringer json = new JSONStringer();
+    json.array();
+    for (GabotoEntity entity : entities) {
+      addEntity(json, entity, 1);
+    }
+    json.endArray();
+    return  json.toString();
+  }
+
+  private void addEntity(JSONStringer json, GabotoEntity entity, int level) {
+    System.err.println("addEntity:" + entity.getUri());
+    // begin new object
+    json.object();
+
+    json.key("uri").value(entity.getUri());
+
+    json.key("type").value(entity.getType());
+
+    if (levelMap.containsKey(entity)) {
+      int storedLevel = levelMap.get(entity).intValue();
+
+      if (storedLevel < level) {
+        json.key("referenced").value(true);
+        json.endObject();
+        return;
       }
-	
-      JSONStringer json = new JSONStringer();
-		
+
+      // store level in map
+      levelMap.put(entity, new Integer(level));
+    }
+
+    if (level > nesting) {
+      json.key("nestingLimitReached").value(true);
+      json.endObject();
+      return;
+    }
+
+    if ((getCollectProperties() & COLLECT_DIRECT_PROPERTIES) == COLLECT_DIRECT_PROPERTIES) {
+      for (Entry<String, Object> entry : entity.getAllDirectProperties().entrySet()) {
+        addMember(json, entry.getKey(), entry.getValue(), level);
+      }
+    }
+
+    if ((getCollectProperties() & COLLECT_INDIRECT_PROPERTIES) == COLLECT_INDIRECT_PROPERTIES) {
+      for (Entry<String, Object> entry : entity.getAllIndirectProperties().entrySet()) {
+        addMember(json, entry.getKey(), entry.getValue(), level);
+      }
+    }
+
+    if ((getCollectProperties() & COLLECT_PASSIVE_PROPERTIES) == COLLECT_PASSIVE_PROPERTIES) {
+      Set<Entry<String, Object>> passiveProperties = entity.getAllPassiveProperties().entrySet();
+      if (passiveProperties.size() > 0) {
+        json.key("passiveProperties");
+        json.object();
+        for (Entry<String, Object> entry : passiveProperties) {
+          addMember(json, entry.getKey(), entry.getValue(), level);
+        }
+        json.endObject();
+      }
+    }
+    // end object
+    json.endObject();
+  }
+
+  @SuppressWarnings("unchecked")
+  private void addMember(JSONStringer json, String memberName, Object memberValue,  int level) 
+      throws JSONException {
+    String key = simplifyKey(memberName); 
+    if (memberValue instanceof String) {
+      json.key(key);
+      json.value(memberValue);
+    } else if (memberValue instanceof GabotoEntity) {
+      json.key(key);
+      addEntity(json, (GabotoEntity) memberValue, level + 1);
+    } else if (memberValue instanceof Collection) {
+      json.key(key);
+      json.array();
+      for (GabotoEntity innerEntity : (Collection<GabotoEntity>) memberValue) {
+        addEntity(json, innerEntity, level + 1);
+      }
+      json.endArray();
+    } else if (memberValue instanceof GabotoBean) {
       try {
+        json.key(key);
+      } catch (JSONException e) {
+        e.printStackTrace();
+        System.err.println();
+        System.err.println("Bean already added " + key);
+        return;
+      }
+      // beans should be put into the same level ..
+      addBean(json, (GabotoBean) memberValue, level);
+    } else if (memberValue == null) {
+      try {
+        json.key(key);
+        json.value(null);
+      } catch (JSONException e) {
+        System.err.println("Null value, already added " + key);
+      }
+    }
+  }
+  @SuppressWarnings("unchecked")
+  private void addBean(JSONStringer json, GabotoBean bean, int level)
+      throws JSONException {
+    if (level > nesting) {
+      json.object();
+      json.key("nestingLimitReached").value(true);
+      json.endObject();
+      return;
+    }
+
+    // begin new object
+    json.object();
+
+    for (Entry<String, Object> entry : bean.getAllProperties().entrySet()) {
+      if (entry.getValue() instanceof String) {
+        json.key(simplifyKey(entry.getKey()));
+        json.value(entry.getValue());
+      } else if (entry.getValue() instanceof GabotoEntity) {
+        json.key(simplifyKey(entry.getKey()));
+        addEntity(json, (GabotoEntity) entry.getValue(), level + 1);
+      } else if (entry.getValue() instanceof Collection) {
+        json.key(simplifyKey(entry.getKey()));
         json.array();
-        for(GabotoEntity entity : entities){
-          transformEntity(entity, json, 1);
+        for (GabotoEntity innerEntity : (Collection<GabotoEntity>) entry
+            .getValue()) {
+          addEntity(json, innerEntity, level + 1);
         }
         json.endArray();
-      } catch (JSONException e) {
-        throw new GabotoRuntimeException(e);
+      } else if (entry.getValue() instanceof GabotoBean) {
+        json.key(simplifyKey(entry.getKey()));
+        addBean(json, (GabotoBean) entry.getValue(), level + 1);
       }
-      return json.toString();
     }
 
-	private void transformEntity(GabotoEntity entity, JSONStringer json, int level) throws JSONException {
-		// store basic knowledge
-		
-		// begin new object
-		json.object();
-		
-		// add uri
-		json.key("uri")
-		    .value(entity.getUri());
-		
-		// add type
-		json.key("type")
-		    .value(entity.getType());
-		
-		if(levelMap.containsKey(entity)){
-			int storedLevel = levelMap.get(entity).intValue();
-			
-			if(storedLevel < level){
-				json.key("referenced").value(true);
-				json.endObject();
-				return;
-			}
-			
-			// store level in map
-			levelMap.put(entity, new Integer(level));
-		}
-		
-		if(level > nesting){
-			json.key("nestingLimitReached").value(true);
-			json.endObject();
-			return;
-		}
-			
-		
-		if((getCollectProperties() & COLLECT_DIRECT_PROPERTIES) == COLLECT_DIRECT_PROPERTIES){
-			for(Entry<String, Object> entry : entity.getAllDirectProperties().entrySet()){
-				processEntityEntry(entry, json, level);
-			}
-		}
-		
-		if((getCollectProperties() & COLLECT_INDIRECT_PROPERTIES) == COLLECT_INDIRECT_PROPERTIES){
-			for(Entry<String, Object> entry : entity.getAllIndirectProperties().entrySet()){
-				processEntityEntry(entry, json, level);
-			}
-		}
-		
-		if((getCollectProperties() & COLLECT_PASSIVE_PROPERTIES) == COLLECT_PASSIVE_PROPERTIES){
-			Set<Entry<String, Object>> passiveProperties = entity.getAllPassiveProperties().entrySet();
-			if(passiveProperties.size() > 0){
-				json.key("passiveProperties");
-				json.object();
-				for(Entry<String, Object> entry : passiveProperties)
-					processEntityEntry(entry, json, level);
-				json.endObject();
-			}
-		}
-		// end object
-		json.endObject();
-	}
-	
-	@SuppressWarnings("unchecked")
-  private void processEntityEntry(Entry<String, Object> entry, JSONStringer json, int level) throws JSONException{
-		if(entry.getValue() instanceof String){
-		    json.key(simplifyKey(entry.getKey()));
-			json.value(entry.getValue());
-		} else if(entry.getValue() instanceof GabotoEntity) {
-		    json.key(simplifyKey(entry.getKey()));
-			transformEntity((GabotoEntity) entry.getValue(), json, level + 1);
-		} else if(entry.getValue() instanceof Collection){
-		    json.key(simplifyKey(entry.getKey()));
-			json.array();
-			for(GabotoEntity innerEntity : (Collection<GabotoEntity>) entry.getValue()){
-				transformEntity(innerEntity, json, level + 1);
-			}
-			json.endArray();
-		} else if(entry.getValue() instanceof GabotoBean){
-          try { 
-            json.key(simplifyKey(entry.getKey()));
-            // beans should be put into the same level ..
-            transformBean((GabotoBean)entry.getValue(), json, level);
-           } catch (JSONException e) { 
-             System.err.println("Already added " + simplifyKey(entry.getKey()));
-           }
-		} else if(entry.getValue() == null){
-          try { 
-            json.key(simplifyKey(entry.getKey()));
-            json.value(null);
-           } catch (JSONException e) { 
-             System.err.println("Already added " + simplifyKey(entry.getKey()));
-           }
-		}
-	}
+    json.endObject();
+  }
 
-	@SuppressWarnings("unchecked")
-  private void transformBean(GabotoBean bean, JSONStringer json, int level) throws JSONException {
-		if(level > nesting){
-			json.object();
-			json.key("nestingLimitReached").value(true);
-			json.endObject();
-			return;
-		}
-			
-		
-		// begin new object
-		json.object();
-		
-		for(Entry<String, Object> entry : bean.getAllProperties().entrySet()){
-			if(entry.getValue() instanceof String){
-			    json.key(simplifyKey(entry.getKey()));
-				json.value(entry.getValue());
-			} else if(entry.getValue() instanceof GabotoEntity) {
-			    json.key(simplifyKey(entry.getKey()));
-				transformEntity((GabotoEntity) entry.getValue(), json, level + 1);
-			} else if(entry.getValue() instanceof Collection){
-			    json.key(simplifyKey(entry.getKey()));
-				json.array();
-				for(GabotoEntity innerEntity : (Collection<GabotoEntity>) entry.getValue()){
-					transformEntity(innerEntity, json, level + 1);
-				}
-				json.endArray();
-			} else if(entry.getValue() instanceof GabotoBean){
-			    json.key(simplifyKey(entry.getKey()));
-				transformBean((GabotoBean) entry.getValue(), json, level + 1);
-			}
-		}
-		
-		// end object
-		json.endObject();
-	}
+  /**
+   * A binary or-ed value, defining which properties are to be added to the JSON
+   * output.
+   * 
+   * <p>
+   * The default is: COLLECT_DIRECT_PROPERTIES | COLLECT_INDIRECT_PROPERTIES |
+   * COLLECT_PASSIVE_PROPERTIES;
+   * </p>
+   * 
+   * @param collectProperties
+   *          defines which properties are collected.
+   * @see JSONPoolTransformer#COLLECT_DIRECT_PROPERTIES
+   * @see JSONPoolTransformer#COLLECT_PASSIVE_PROPERTIES
+   * @see JSONPoolTransformer#COLLECT_INDIRECT_PROPERTIES
+   */
+  public void setCollectProperties(int collectProperties) {
+    this.collectProperties = collectProperties;
+  }
 
-	/**
-	 * A binary or-ed value, defining which properties are to be added to the JSON output.
-	 * 
-	 * <p>
-	 * The default is:
-	 * COLLECT_DIRECT_PROPERTIES | COLLECT_INDIRECT_PROPERTIES | COLLECT_PASSIVE_PROPERTIES;
-	 * </p>
-	 * 
-	 * @param collectProperties defines which properties are collected.
-	 * @see JSONPoolTransformer#COLLECT_DIRECT_PROPERTIES
-	 * @see JSONPoolTransformer#COLLECT_PASSIVE_PROPERTIES
-	 * @see JSONPoolTransformer#COLLECT_INDIRECT_PROPERTIES
-	 */
-	public void setCollectProperties(int collectProperties) {
-		this.collectProperties = collectProperties;
-	}
+  /**
+   * Returns which properties are to be collected.
+   * 
+   * @return which properties are to be collected.
+   */
+  public int getCollectProperties() {
+    return collectProperties;
+  }
 
-	/**
-	 * Returns which properties are to be collected.
-	 * 
-	 * @return which properties are to be collected.
-	 */
-	public int getCollectProperties() {
-		return collectProperties;
-	}
+  /**
+   * @param nesting
+   *          the nesting level
+   */
+  public void setNesting(int nesting) {
+    if (nesting < 1 || nesting > MAX_NESTING)
+      throw new IllegalArgumentException("Nesting has to be between 0 and "
+          + MAX_NESTING);
+    this.nesting = nesting;
+  }
 
-	/**
-	 * @param nesting the nesting level 
-	 */
-	public void setNesting(int nesting) {
-		if(nesting < 1 || nesting > MAX_NESTING)
-			throw new IllegalArgumentException("Nesting has to be between 0 and " + MAX_NESTING);
-		this.nesting = nesting;
-	}
+  /**
+   * @return the nesting
+   */
+  public int getNesting() {
+    return nesting;
+  }
 
-	/**
-	 * @return the nesting
-	 */
-	public int getNesting() {
-		return nesting;
-	}
+
+  private String simplifyKey(String k) {
+    k = k.replaceAll("http://ns.ox.ac.uk/namespace/oxpoints/2009/02/owl#",
+        "oxp_");
+    k = k.replaceAll("http://ns.ox.ac.uk/namespace/gaboto/kml/2009/03/owl#",
+        "gab_");
+    k = k.replaceAll("http://purl.org/dc/elements/1.1/", "dc_");
+    k = k.replaceAll("http://nwalsh.com/rdf/vCard#", "vCard_");
+    k = k.replaceAll("http://www.opengis.net/gml/", "geo_");
+    return k;
+  }
+
+
 }
