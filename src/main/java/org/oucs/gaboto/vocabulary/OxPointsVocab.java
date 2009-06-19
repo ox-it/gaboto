e30b4397b8dc30b6aea408d31b8bc449bbcad138
Tim Pizey <tim.pizey@oucs.ox.ac.uk>
2009-06-19 09:09:17 +0000

    Organise Imports

--- src/main/java/org/oucs/gaboto/vocabulary/OxPointsVocab.java
+++ src/main/java/org/oucs/gaboto/vocabulary/OxPointsVocab.java
@@ -1,7 +1,11 @@
 /* CVS $Id: $ */
 package org.oucs.gaboto.vocabulary; 
-import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.ontology.*;
+import com.hp.hpl.jena.ontology.ObjectProperty;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
  
 /**
  * Vocabulary definitions from ontologies/oxpoints.owl 
