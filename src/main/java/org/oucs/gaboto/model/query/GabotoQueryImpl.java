eefb91490a2c85848a6cb11379c491ec2cd25861
Tim Pizey <tim.pizey@oucs.ox.ac.uk>
2009-07-23 09:11:57 +0000

    Move exception

--- src/main/java/org/oucs/gaboto/model/query/GabotoQueryImpl.java
+++ src/main/java/org/oucs/gaboto/model/query/GabotoQueryImpl.java
@@ -40,7 +40,6 @@ import org.oucs.gaboto.entities.pool.GabotoEntityPoolConfiguration;
 import org.oucs.gaboto.exceptions.GabotoException;
 import org.oucs.gaboto.exceptions.GabotoRuntimeException;
 import org.oucs.gaboto.exceptions.QueryAlreadyPreparedException;
-import org.oucs.gaboto.exceptions.UnsupportedQueryFormatException;
 import org.oucs.gaboto.model.Gaboto;
 import org.oucs.gaboto.transformation.RDFPoolTransformerFactory;
 import org.oucs.gaboto.transformation.json.JSONPoolTransformer;
